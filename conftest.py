5ea62daeb287920d16a4c8f2fc4e3daf6dd4660b
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-02-03 15:43:26 +0200

    Mantis

---
+++ conftest.py
@@ -0,0 +1,89 @@
+import pytest
+from fixture.application import Application
+import json
+import os.path
+import jsonpickle
+import ftputil
+
+fixture = None
+target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture
+def app(request, config):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = config
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, config=config)
+    fixture.session.ensure_login(username=web_config["webadmin"]["username"],
+                                 password=web_config["webadmin"]["password"])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+
+    request.addfinalizer(fin)
+    return fixture
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+
+    def fin():
+        restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+
+    request.addfinalizer(fin)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
---
+++ fixture/application.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
+from fixture.soap import SoapHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
+
+
+class Application:
+
+    def __init__(self, browser, config):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.config = config
+        self.base_url = config['web']['baseURL']
+        self.session = SessionHelper(self)
+        self.project = ProjectHelper(self)
+        self.james = JamesHelper(self)
+        self.soap = SoapHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self, wd):
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
