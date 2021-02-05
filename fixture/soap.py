c87c1930abc06e8d286f6bf003b82e5f778a759e
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-02-05 17:50:10 +0200

    Исправлено

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,17 +1,20 @@
 from suds.client import Client
 from suds import WebFault
 from fixture.project import Project
-#tr
+from fixture.application import Application
+
+
+# tr
 
 class SoapHelper:
-
     project_cache = None
 
     def __init__(self, app):
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        #client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config['web']['baseUrl'] + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -19,8 +22,8 @@ class SoapHelper:
             return False
 
     def get_project_list(self):
-        self.project_cache =[]
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        self.project_cache = []
+        client = Client(self.app.config['web']['baseUrl'] + "/api/soap/mantisconnect.php?wsdl")
         projects = client.service.mc_projects_get_user_accessible(self.app.config["webadmin"]["username"],
                                                                   self.app.config["webadmin"]["password"])
         for project in projects:
